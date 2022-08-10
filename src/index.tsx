7dcb47766e4c5bf8d4612655095153d157c05b03
Volodimir <vova19941@ukr.net>
2022-08-10 19:50:58 +0300

    add config ts file

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
+import App from './container/App/App'
 
-
-
-function App() {
-  return(
-    <>
-      <div>
-          <h1>Hello World</h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus deleniti quo adipisci officiis assumenda, enim porro in, consequuntur, voluptatem ad amet aliquid quis recusandae dolorem nisi. Vero laborum magni, delectus tempore nobis vel consequatur iure, quaerat labore suscipit fuga repellat tenetur sequi minus animi nisi aliquam. Incidunt doloribus sit cupiditate.</p>
-      </div>
-      <div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ad quae quas fugiat illo fugit debitis labore ipsum excepturi omnis?</p>
-      </div>
-      </>
-  )
-}
-
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(<App/>);
- 
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<App />)
