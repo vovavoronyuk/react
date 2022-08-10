81cdd1d0bd5a05f1accc799ca223467b5d3be67a
Volodimir <vova19941@ukr.net>
2022-08-10 20:12:37 +0300

    add instal MUI

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,29 +1,9 @@
 import React from 'react'
 
-type Props = {
-    title: string
-}
-
-const Applist = () => {
-    return (
-        <ul>
-            <li>list item 1</li>
-            <li>list item 2</li>
-            <li>list item 3</li>
-        </ul>
-    )
-}
-
-const AppHeader = (props: Props) => {
-    return <h1>Hello {props.title}</h1>
-}
-
 const App = () => {
     return (
         <>
-            <AppHeader title="App.js" />
-            <AppHeader title="React.js" />
-            <Applist />
+            <h1>Hello App</h1>
         </>
     )
 }
