c881572ffbaf4a2055a16e0f5c7617ead19a17e2
Volodimir <vova19941@ukr.net>
2022-08-10 18:56:23 +0300

    add react tag

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-const h1=<h1>Hello World</h1>
+
 
 function App() {
   return(
+    <>
       <div>
-          {h1}
+          <h1>Hello World</h1>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus deleniti quo adipisci officiis assumenda, enim porro in, consequuntur, voluptatem ad amet aliquid quis recusandae dolorem nisi. Vero laborum magni, delectus tempore nobis vel consequatur iure, quaerat labore suscipit fuga repellat tenetur sequi minus animi nisi aliquam. Incidunt doloribus sit cupiditate.</p>
       </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ad quae quas fugiat illo fugit debitis labore ipsum excepturi omnis?</p>
+      </div>
+      </>
   )
 }
 
