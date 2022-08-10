122d7593386dc146a929cc9b35eb7f87bead844f
Volodimir <vova19941@ukr.net>
2022-08-10 20:44:27 +0300

    add header

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import CssBaseline from '@mui/material/CssBaseline'
+import Header from 'container/Header/Header'
 const App = () => {
     return (
         <>
-            <h1>Hello App</h1>
+            <CssBaseline />
+            <Header />
         </>
     )
 }
---
+++ src/container/Header/Header.tsx
@@ -0,0 +1,43 @@
+// tsrafce це запуск компонента
+import React from 'react'
+
+import {
+    Container,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    Button,
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+type Props = {}
+
+const Header = (props: Props) => {
+    return (
+        <AppBar position="static">
+            <Container>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        Fake Shop
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
+}
+
+export default Header
