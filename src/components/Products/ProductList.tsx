00413f461501e388306bff8a5f120fe7ff20702c
Volodimir <vova19941@ukr.net>
2022-08-15 20:35:24 +0300

    add product props

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -18,13 +18,31 @@ const ProductList = (props: Props) => {
                 spacing={3}
             >
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        name="iPhone X"
+                        description="This is iPhone XS"
+                        type="phone"
+                        capacity={64}
+                        price={500}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        name="iPhone 11"
+                        description="This is iPhone 11"
+                        type="phone"
+                        capacity={128}
+                        price={600}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        name="iPhone 12 Pro"
+                        description="This is iPhone 12 Pro"
+                        type="phone"
+                        capacity={256}
+                        price={700}
+                    />
                 </Grid>
             </Grid>
         </>
--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import { Button, Card, CardActions, CardContent } from '@mui/material'
-type Props = {}
 
-const ProductListItem = (props: Props) => {
+type ProductProps = {
+    name: string
+    description: string
+    type: string
+    capacity: number
+    price: number
+}
+
+const ProductListItem = ({
+    name,
+    description,
+    type,
+    capacity,
+    price,
+}: ProductProps) => {
     return (
         <Card>
             <CardContent>
-                <h4 className="product-title">iPhone X</h4>
-                <div className="product-description">This is iPhone X</div>
-                <div className="product-features">Type: phone</div>
-                <div className="product-features">Capacity:64GB</div>
-                <div className="product-price">$ 500</div>
+                <h4 className="product-title">{name}</h4>
+                <div className="product-description">{description}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity:{capacity}</div>
+                <div className="product-price">$ {price}</div>
             </CardContent>
             <CardActions>
                 <Button variant="contained">Add to cart</Button>
