1dbb4311c331fc8ad7c677e6ab97fbcdef396fc6
Volodimir <vova19941@ukr.net>
2022-08-22 19:50:09 +0300

    add const this.props

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -24,7 +24,7 @@ type State = {
 
 class ProductListItem extends Component<ProductProps, State> {
     state = {
-        count: 1,
+        count: 100,
     }
     // constructor(props: ProductProps) {
     //     super(props)
@@ -34,23 +34,18 @@ class ProductListItem extends Component<ProductProps, State> {
     // }
 
     render() {
+        const { image, name, description, type, capacity, price } = this.props
         return (
             <Card>
                 <CardContent>
                     <div className="product-img">
-                        <img src={this.props.image} alt="" />
+                        <img src={image} alt="" />
                     </div>
-                    <h4 className="product-title">{this.props.name}</h4>
-                    <div className="product-description">
-                        {this.props.description}
-                    </div>
-                    <div className="product-features ">
-                        Type: {this.props.type}
-                    </div>
-                    <div className="product-features">
-                        Capacity:{this.props.capacity}
-                    </div>
-                    <div className="product-price">$ {this.props.price}</div>
+                    <h4 className="product-title">{name}</h4>
+                    <div className="product-description">{description}</div>
+                    <div className="product-features ">Type: {type}</div>
+                    <div className="product-features">Capacity:{capacity}</div>
+                    <div className="product-price">$ {price}</div>
                     <div className="product-quantity">
                         <Button variant="contained">-</Button>
                         <TextField
